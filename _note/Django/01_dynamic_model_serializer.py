b764c2310455cafeb7f67d820ac682ec41e8b2b0
kyle <renine94.dev@gmail.com>
2022-11-07 19:51:42 +0900

    [Note] Django - Serializer : GET요청시 query_param 으로 동적 데이터 가져오기

---
+++ _note/Django/01_dynamic_model_serializer.py
@@ -0,0 +1,19 @@
+class DynamicFieldsModelSerializer(ModelSerializer):
+    """
+    https://stackoverflow.com/questions/23643204/django-rest-framework-dynamically-return-subset-of-fields
+    A ModelSerializer that takes an additional `fields` argument that
+    controls which fields should be displayed.
+    """
+
+    def __init__(self, *args, **kwargs):
+        # Instantiate the superclass normally
+        super(DynamicFieldsModelSerializer, self).__init__(*args, **kwargs)
+
+        fields = self.context['request'].query_params.get('fields')
+        if fields:
+            fields = fields.split(',')
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields.keys())
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
