ac078b5c5d9d737a24ed63acd016b8429c11e7ec
kyle <renine94.dev@gmail.com>
2022-12-15 16:20:17 +0900

    [Note] ES - 한글분석기 :: 은전한 닢 :: 사용예제와 index_poses 태그의 의미, 명사사전, 유사어 같이사용하기

--- _note/ElasticSearch/09_korean_analyzer.py
+++ _note/ElasticSearch/09_korean_analyzer.py
@@ -5,12 +5,14 @@
 
 # 한글 분석기 중 유명한 것 2가지
 # 1. nori_analyzer (es default 한글분석기)
-
+pass
 
 
 
 
 # 2. 은전한잎 (opensearch)
+# https://github.com/codelibs/elasticsearch-analysis-seunjeon/blob/master/elasticsearch/README.md
+# https://velog.io/@discphy/elasticsearch
 {
   "settings": {
     "analysis": {
@@ -59,4 +61,97 @@
       }
     }
   }
+}
+
+########################################################################
+# 은전한 닢 + 유사어 사전 동시에 활용하면서 index_poses 관리
+# 치악 이라는 단어를 명사사전으로 등록해서 [치악/N] 타입으로 만들어줘야 한다.
+########################################################################
+# GET /_analyze
+{
+  "char_filter": [
+      {
+        "type": "pattern_replace",
+        "pattern": "\\s+",
+        "replacement": ""
+      }
+  ],
+  "tokenizer": {
+    "type" : "seunjeon_tokenizer",
+    # https://adipo.tistory.com/entry/%ED%92%88%EC%82%AC%E5%93%81%E8%A9%9E-5%EC%96%B8-9%ED%92%88%EC%82%AC-%EC%B2%B4%EC%96%B8-%EC%9A%A9%EC%96%B8-%EA%B4%80%EA%B3%84%EC%96%B8-%EC%88%98%EC%8B%9D%EC%96%B8-%EB%8F%85%EB%A6%BD%EC%96%B8
+    "index_poses": ["UNK", "EP", "E", "I", "J", "M", "N", "S", "SL", "SH", "SN", "V", "VCP", "XP", "XS", "XR", "V"],
+    "user_words": [
+      "내추럴발란스",
+      "치악"  # TODO 이 부분을 등록해줘야 에러없이 분석이 된다. 없으면 [치악/EOJ] 형태로 분석되어서 에러가 난다. (EOJ: 어절입니다. 명사+조사가 한 어절을 이루었네요.)
+    ]
+  },
+  "filter": [
+    {
+      "type": "synonym",
+      "synonyms": [
+        "치악 => 치약",
+        "내츄럴, 네츄럴, 네추럴 => 내추럴"
+      ]
+    }
+  ],
+  "text": ["치악"]
+}
+
+################################
+# GET /_analyze
+{
+  "char_filter": [
+      {
+        "type": "pattern_replace",
+        "pattern": "\\s+",
+        "replacement": ""
+      }
+  ],
+  "tokenizer": {
+    "type" : "seunjeon_tokenizer",
+    "index_poses": ["UNK", "EP", "E", "I", "J", "M", "N", "S", "SL", "SH", "SN", "V", "VCP", "XP", "XS", "XR"],
+    "user_words": [
+      "내추럴발란스",
+      "치악", "치억"
+    ]
+  },
+  "filter": [
+    {
+      "type": "synonym",
+      "synonyms": [
+        "치악, 치억, 치약",
+        "내츄럴, 네츄럴, 네추럴 => 내추럴"
+      ]
+    }
+  ],
+  "text": ["치악"]
+}
+#################################
+# GET /_analyze
+{
+  "char_filter": [
+      {
+        "type": "pattern_replace",
+        "pattern": "\\s+",
+        "replacement": ""
+      }
+  ],
+  "tokenizer": {
+    "type" : "seunjeon_tokenizer",
+    "index_poses": ["UNK", "EP", "E", "I", "J", "M", "N", "S", "SL", "SH", "SN", "V", "VCP", "XP", "XS", "XR"],
+    "user_words": [
+      "내추럴발란스",
+      "치악"
+    ]
+  },
+  "filter": [
+    {
+      "type": "synonym",
+      "synonyms": [
+        "치악 => 치약",
+        "내츄럴, 네츄럴, 네추럴 => 내추럴"
+      ]
+    }
+  ],
+  "text": ["NEW 캐츠랑 전연령 점보 (20kg)"]
 }
