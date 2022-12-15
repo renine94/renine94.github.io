ad96a0a92b0f99e84de5147837cf89358e2287e6
kyle <renine94.dev@gmail.com>
2022-12-15 16:23:26 +0900

    [Note] ES :: reindex :: 로컬과 원격에서 재인덱싱 하기 명령어

--- _note/ElasticSearch/index/02_reindex.py
+++ _note/ElasticSearch/index/02_reindex.py
@@ -1,4 +1,7 @@
 # https://docs.aws.amazon.com/opensearch-service/latest/developerguide/custom-packages.html?icmpid=docs_console_unmapped#custom-packages-gs
+# https://devpouch.tistory.com/105 - 다른 원격 클러스터에서 현재 클러스터로 reindex
+
+
 # 새로운 인덱스를 만들고 재색인을 할 때 사용한다.
 
 
@@ -9,5 +12,26 @@
   },
   "dest": {
     "index": "my-new-index"
+  }
+}
+
+
+# POST _reindex?wait_for_completion=true
+{
+  "source": {
+    "remote": {
+      "host": "http://otherhost:9200",  # 443
+      "username": "user",
+      "password": "pass"
+    },
+    "index": "machine_log",
+    "query": {
+      "match": {
+        "test": "data"
+      }
+    }
+  },
+  "dest": {
+    "index": "another_machine_log"
   }
 }
